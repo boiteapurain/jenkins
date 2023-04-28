048c000ac81699b4471c8abad0ee6b14332118e6
boiteapurain <mathieu.negrel82@gmail.com>
2023-04-29 00:50:18 +0200

    add test

--- package.py
+++ package.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
 name = "jenkins_test"
 
+require = [
+    "pyLib-3.10",
+    "python-3.10",
+]
+
 
 def commands():
     global env
 
-    # pythonpath
+    env.PATH.append("{root}/tests")
+
     env.PYTHONPATH.append("{root}/python")
+    env.PYTHONPATH.append("{root}/tests")
